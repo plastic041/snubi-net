289635d99b98bf70e4471fb22b0256c7583baf32
jaewoo_byeon2 <jaewoo_byeon2@tmax.co.kr>
2022-09-14 18:37:03 +0900

    use tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import type { NextPage } from "next";
-import { tw } from "twind";
 
 const Home: NextPage = () => {
-  return <div className={`${tw`container bg-blue-500 h-screen`}`}></div>;
+  return <div className="container bg-blue-500 h-screen mx-auto">hi</div>;
 };
 
 export default Home;

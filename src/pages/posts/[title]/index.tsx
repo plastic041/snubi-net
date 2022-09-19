a7bc4437df0c140b30368d6b36751e91ceaee843
plastic041 <plastic041@gmail.com>
2022-09-20 07:44:35 +0900

    framer motion

--- src/pages/posts/[title]/index.tsx
+++ src/pages/posts/[title]/index.tsx
@@ -1,4 +1,5 @@
 import { ArrowUturnLeftIcon } from "@heroicons/react/24/outline";
+import { motion } from "framer-motion";
 import { readFile, readdir } from "fs/promises";
 import matter from "gray-matter";
 import type { GetStaticPaths, GetStaticProps } from "next";
@@ -105,36 +106,39 @@ const PostPage = ({ html, frontmatter }: Props) => {
   const title = `${frontmatter.title} | Snubi.net`;
 
   return (
-    <>
-      <Layout title={title} description={frontmatter.description}>
-        <article className="flex flex-1 flex-col gap-16 p-4 lg:grid lg:grid-cols-3">
-          <div className="top-2 col-span-1 flex flex-col gap-2 self-start lg:sticky -mt-8">
-            <Link href="/posts">
-              <a className="flex gap-2 items-center text-gray-500 transition-color hover:text-gray-900 duration-200 dark:text-gray-200 dark:hover:text-gray-400 self-start">
-                <ArrowUturnLeftIcon className="w-4 h-4" />
-                <span>목록으로</span>
-              </a>
-            </Link>
-            <h1 className="break-words text-4xl font-extrabold [word-break:keep-all] text-gray-900 dark:text-gray-100">
-              {frontmatter.title}
-            </h1>
-            <p className="text-gray-700 hidden lg:block dark:text-gray-200">
-              {frontmatter.description}
-            </p>
-            <time
-              dateTime={frontmatter.created_at}
-              className="text-gray-700 dark:text-gray-200"
-            >
-              {frontmatter.created_at}
-            </time>
-          </div>
-          <div
-            className="prose col-span-2 dark:prose-invert break-words [word-break:keep-all] whitespace-pre-wrap flex flex-col"
-            dangerouslySetInnerHTML={{ __html: html }}
-          />
-        </article>
-      </Layout>
-    </>
+    <Layout title={title} description={frontmatter.description}>
+      <article className="flex flex-1 flex-col gap-16 p-4 lg:grid lg:grid-cols-3">
+        <motion.div
+          className="top-2 col-span-1 flex flex-col gap-2 self-start lg:sticky -mt-8"
+          initial={{ y: -10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+        >
+          <Link href="/posts">
+            <a className="flex gap-2 items-center text-gray-500 transition-color hover:text-gray-900 duration-200 dark:text-gray-200 dark:hover:text-gray-400 self-start">
+              <ArrowUturnLeftIcon className="w-4 h-4" />
+              <span>목록으로</span>
+            </a>
+          </Link>
+          <h1 className="break-words text-4xl font-extrabold [word-break:keep-all] text-gray-900 dark:text-gray-100">
+            {frontmatter.title}
+          </h1>
+          <p className="text-gray-700 hidden lg:block dark:text-gray-200">
+            {frontmatter.description}
+          </p>
+          <time
+            dateTime={frontmatter.created_at}
+            className="text-gray-700 dark:text-gray-200"
+          >
+            {frontmatter.created_at}
+          </time>
+        </motion.div>
+        <div
+          className="prose col-span-2 dark:prose-invert break-words [word-break:keep-all] whitespace-pre-wrap flex flex-col"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </article>
+    </Layout>
   );
 };
 

9de4f8232a14f4151713986b81f32f7185e4feb9
plastic041 <plastic041@gmail.com>
2022-09-25 21:40:02 +0900

    불필요한 테스트 삭제

--- __tests__/sum.test.ts
+++
@@ -1,8 +0,0 @@
-// import { describe, expect, it } from "vitest";
-// import { sum } from "~/lib/use-is-active";
-
-// describe("sum", () => {
-//   it("should add two numbers", () => {
-//     expect(sum(1, 2)).toBe(3);
-//   });
-// });

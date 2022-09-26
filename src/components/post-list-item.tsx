08e8960272b421054ddc69561464d27e70b67aa7
jaewoo byeon <jaewoo_byeon2@tmax.co.kr>
2022-09-26 16:31:58 +0900

    tag link

--- src/components/post-list-item.tsx
+++ src/components/post-list-item.tsx
@@ -8,27 +8,29 @@ type PostItemProps = {
 const PostItem = ({ frontmatter }: PostItemProps) => {
   return (
     <li className="flex flex-col">
-      <Link href={`/posts/${frontmatter.slug}`}>
-        <a className="flex flex-col rounded-lg p-4 transition-colors hover:bg-gray-100 dark:hover:bg-gray-700 lg:p-8">
-          <h3 className="text-2xl font-bold text-gray-900 dark:text-gray-100">
-            {frontmatter.title}
-          </h3>
-          <span className="mb-2 text-gray-700 dark:text-gray-200">
-            {frontmatter.description}
-          </span>
-          <time
-            className="mb-2 text-gray-700 dark:text-gray-200"
-            dateTime={frontmatter.created_at}
-          >
-            {frontmatter.created_at}
-          </time>
-          <div className="flex flex-row gap-1">
-            {frontmatter.tags.map((tag) => (
-              <Tag tag={tag} key={tag} />
-            ))}
-          </div>
-        </a>
-      </Link>
+      <section className="flex flex-col rounded-lg p-4 transition-colors hover:bg-gray-100 dark:hover:bg-gray-700 lg:p-8">
+        <Link href={`/posts/${frontmatter.slug}`}>
+          <a className="flex flex-col">
+            <h3 className="text-2xl font-bold text-gray-900 dark:text-gray-100">
+              {frontmatter.title}
+            </h3>
+            <span className="mb-2 text-gray-700 dark:text-gray-200">
+              {frontmatter.description}
+            </span>
+            <time
+              className="mb-2 text-gray-700 dark:text-gray-200"
+              dateTime={frontmatter.created_at}
+            >
+              {frontmatter.created_at}
+            </time>
+          </a>
+        </Link>
+        <div className="flex flex-row gap-1">
+          {frontmatter.tags.map((tag) => (
+            <Tag tag={tag} key={tag} isAnchor />
+          ))}
+        </div>
+      </section>
     </li>
   );
 };

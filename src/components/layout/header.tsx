da55f3b3ad42ea6fbe6fc427bb5b92a096b2a0b3
jaewoo byeon <jaewoo_byeon2@tmax.co.kr>
2022-09-30 12:53:47 +0900

    works list

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -13,8 +13,8 @@ const CategoryItem = ({ name, href }: CategoryProps) => {
     <Link href={href}>
       <a className="flex">
         <span
-          className={`text-blue-500 underline-offset-2 transition-colors hover:text-blue-700 dark:text-blue-300 dark:hover:text-blue-400
-          ${isActive ? "underline" : ""}
+          className={`border-blue-500 text-blue-500 underline-offset-2 transition-colors hover:text-blue-700 dark:border-blue-300 dark:text-blue-300 dark:hover:text-blue-400
+          ${isActive ? "border-b-2" : ""}
         `}
         >
           {name}
--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,13 +1,33 @@
-import ConstructionCat from "~/../public/images/construction-cat.png";
-import HeroImage from "~/components/hero-image";
+import Image from "next/future/image";
+import Link from "next/link";
 import Layout from "~/components/layout";
 
 const WorksPage = () => {
   return (
     <Layout title="작업 | Snubi" description="작업 모음">
       <div className="flex flex-col items-center gap-4 p-8">
-        <HeroImage src={ConstructionCat} alt="안전모를 쓴 고양이" />
-        <p className="text-gray-500 dark:text-gray-400">공사중입니다.</p>
+        <Link href="/works/destiny-card">
+          <a className="flex flex-col items-center gap-4 border border-gray-300 p-4 transition-colors dark:border-gray-400 hover:dark:bg-gray-600">
+            <Image
+              src="/images/destiny-card/thumbnail.png"
+              alt="destiny-card"
+              width={400}
+              height={400}
+              className="w-64"
+            />
+            <div className="flex flex-col items-center gap-2">
+              <span className="text-xl font-bold text-gray-900 dark:text-gray-50">
+                데스티니 카드 생성기
+              </span>
+              <time
+                className="text-gray-700 dark:text-gray-300"
+                dateTime="2022-09-30"
+              >
+                2022년 9월 30일
+              </time>
+            </div>
+          </a>
+        </Link>
       </div>
     </Layout>
   );

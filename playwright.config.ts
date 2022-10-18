f658f91885c4bd1906e407969c5fea8f25939eff
plastic041 <plastic041@gmail.com>
2022-10-18 16:02:27 +0900

    test ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -100,7 +100,7 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "yarn build && yarn start",
+    command: "yarn start",
     port: 3000,
   },
 };

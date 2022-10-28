5f64fd3a097398be3fb9710f997bfd4daa670697
plastic041 <plastic041@gmail.com>
2022-10-28 17:35:00 +0900

    s

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,7 @@ import HeroImage from "~/components/hero-image";
 const Home = () => {
   return (
     <>
-      <div
-        className="aria-asc:bg-blue-700 flex flex-col items-center gap-4 p-8"
-        aria-checked
-      >
+      <div className="flex flex-col items-center gap-4 p-8">
         <HeroImage src={heroCatPic} alt="메롱하는 귀여운 턱시도 고양이, 유화" />
       </div>
     </>

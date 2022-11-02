2f7a8ca1f6fb701963ca302d08976d65b80d6bd6
jaewoo byeon <jaewoo_byeon2@tmax.co.kr>
2022-11-02 18:47:29 +0900

    use different paths for screenshot testing

--- e2e/destiny-page.test.ts
+++ e2e/destiny-page.test.ts
@@ -1,9 +1,11 @@
 import { test, expect } from "@playwright/test";
+import { resolve } from "path";
 
-test("works/destiny-card page works", async ({ page }) => {
+test("works/destiny-card page works", async ({ page }, testInfo) => {
   await page.goto("works/destiny-card");
 
   await page.waitForTimeout(1000);
+  testInfo.snapshotDir = resolve(__dirname, "destiny-card"); //  in current test folder creates new folder named "a.ts-snapshots"
 
   await expect(page).toHaveScreenshot();
 });

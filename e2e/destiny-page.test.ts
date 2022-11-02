b669dedae10a35efc931c2836a19b9d641b7f08b
plastic041 <plastic041@gmail.com>
2022-11-02 19:52:43 +0900

    image

--- e2e/destiny-page.test.ts
+++ e2e/destiny-page.test.ts
@@ -5,7 +5,14 @@ test("works/destiny-card page works", async ({ page }, testInfo) => {
   await page.goto("works/destiny-card");
 
   await page.waitForTimeout(1000);
-  testInfo.snapshotDir = resolve(__dirname, "destiny-card"); //  in current test folder creates new folder named "a.ts-snapshots"
+
+  const screenshot = await page.screenshot();
+
+  testInfo.snapshotDir = resolve(__dirname, "destiny-card");
+  testInfo.attach("screenshot", {
+    body: screenshot,
+    contentType: "image/png",
+  });
 
   await expect(page).toHaveScreenshot();
 });

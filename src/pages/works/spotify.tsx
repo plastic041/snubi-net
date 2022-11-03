e221a5498ef6ddda60a678b799590fdfe4db93be
jaewoo byeon <jaewoo_byeon2@tmax.co.kr>
2022-11-03 11:22:47 +0900

    OgHead Fixes #109

--- src/pages/works/spotify.tsx
+++ src/pages/works/spotify.tsx
@@ -1,16 +1,21 @@
 import { SpotifyIcon } from "~/components/icons";
 import Layout from "~/components/layout";
 import { NowPlaying, NowPlayingIcon } from "~/components/now-playing";
+import { OgHead } from "~/components/og";
 import { TopTracks } from "~/components/top-tracks";
+import { type Og } from "~/typings/og";
 
-const pageInfo = {
-  title: "Top Tracks | Snubi",
-  description: "Top Tracks",
+const og: Og = {
+  title: "스포티파이 재생 목록 | Snubi",
+  description: "Snubi의 스포티파이 재생 목록입니다.",
+  image: "https://snubi-net.vercel.app/images/hero-cat.png",
+  url: "https://snubi-net.vercel.app/works/spotify",
 };
 
 const TopTracksPage = () => {
   return (
-    <Layout {...pageInfo}>
+    <Layout {...og}>
+      <OgHead og={og} />
       <div className="flex flex-col gap-8 p-8">
         <section className="flex flex-col gap-4">
           <div className="flex items-center gap-4 text-gray-900 dark:text-gray-100">

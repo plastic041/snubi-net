530a7576cd74bc896bf5f1dd05a8598cb1a57469
jaewoo byeon <jaewoo_byeon2@tmax.co.kr>
2022-11-03 15:09:28 +0900

    use-is-active 수정 Fixes #108

--- src/lib/hooks/use-is-active.ts
+++ src/lib/hooks/use-is-active.ts
@@ -3,5 +3,8 @@ import { useRouter } from "next/router";
 export const useIsActive = (href: string) => {
   const router = useRouter();
 
-  return router.pathname.startsWith(href);
+  const currentPathname = router.pathname.split("/")[1]; // remove the first slash
+  const isActive = currentPathname === href.split("/")[1];
+
+  return isActive;
 };

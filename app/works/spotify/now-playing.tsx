160acc1a249b5ae717ec670e7220b070c3529985
plastic041 <plastic041@gmail.com>
2023-03-20 14:54:52 +0900

    remove got, use buffer

--- app/works/spotify/now-playing.tsx
+++ app/works/spotify/now-playing.tsx
@@ -3,23 +3,24 @@ import { TrackCard } from "~/components/track-card";
 import { getNowPlaying } from "./fetcher";
 import { type SpotifyNowPlayingSong } from "~/typings/spotify";
 
+const validateIsPlaying = (
+  nowPlaying: Awaited<ReturnType<typeof getNowPlaying>>
+): nowPlaying is SpotifyNowPlayingSong => {
+  const isPlaying = nowPlaying ? nowPlaying.isPlaying : false;
+  return isPlaying;
+};
+
 export const NowPlayingIcon = async () => {
   const nowPlaying = await getNowPlaying();
+  const isPlaying = validateIsPlaying(nowPlaying);
 
-  if (!nowPlaying) {
+  if (!isPlaying) {
     return (
       <SpotifyIcon className="h-8 w-8 text-gray-300 transition-colors dark:text-gray-500" />
     );
   }
 
   return <SpotifyIcon className="h-8 w-8 text-[#1DB954] transition-colors" />;
-};
-
-const validateIsPlaying = (
-  nowPlaying: Awaited<ReturnType<typeof getNowPlaying>>
-): nowPlaying is SpotifyNowPlayingSong => {
-  const isPlaying = nowPlaying ? nowPlaying.isPlaying : false;
-  return isPlaying;
 };
 
 export const NowPlaying = async () => {

0ca89b9960831edf1c6168ec62714d78e49b1714
plastic041 <plastic041@gmail.com>
2023-10-16 17:57:23 +0900

    feat: vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), mdx()],
   output: "hybrid",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });

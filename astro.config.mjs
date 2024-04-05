5e66a17e4bae98e4b4d620e44c57ed3561ba1442
plastic041 <plastic041@gmail.com>
2024-04-05 13:35:52 +0900

    Add web analytics to Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind(), react(), mdx()],
   output: "hybrid",
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });

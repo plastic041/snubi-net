b278c57aa450602cb4cd4b7f37480601f721729d
plastic041@ <plastic041@gmail.com>
2024-12-01 21:55:30 +0900

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
+  site: "https://snubi-net.vercel.app/",
+  integrations: [tailwind(), react(), mdx(), sitemap()],
   output: "hybrid",
   adapter: vercel({
     webAnalytics: { enabled: true },
